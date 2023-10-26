a79f19123c6fc56814735656768c1df6563b446f
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-10-26 15:09:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,25 +58,3 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-try:
-    # Streamlit < 0.65
-    from streamlit.ReportThread import get_report_ctx
-
-except ModuleNotFoundError:
-    try:
-        # Streamlit > 0.65
-        from streamlit.report_thread import get_report_ctx
-
-    except ModuleNotFoundError:
-        try:
-            # Streamlit > ~1.3
-            from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
-
-        except ModuleNotFoundError:
-            try:
-                # Streamlit > ~1.8
-                from streamlit.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
-
-            except ModuleNotFoundError:
-                # Streamlit > ~1.12
-                from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
