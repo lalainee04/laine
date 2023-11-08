5884c39a3f76b7779344ee5e9b36d06a49ac0dba
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-08 18:29:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,9 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
+# don't run anything past here while we troubleshoot 
+streamlit.stop()
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
