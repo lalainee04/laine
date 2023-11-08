0cbcddbc0fac023a47ce9375133417665736a2c8
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-08 19:25:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit 
+import pandas 
+import request 
+import snowflake.connector 
+from urllib.error import URLError 
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,7 +14,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -29,7 +33,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -64,7 +68,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 
-import snowflake.connector
+#import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
