1a840fe3a5d47a3da5895c823d82b288b04e1181
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-12 18:45:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 #import snowflake.connector
 # Allow the end user to add a fruit to the list 
