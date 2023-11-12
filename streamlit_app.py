d42ca57520b8c0b18012f7c0bc12b053829da16f
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-12 17:46:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
 
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import pandas 
@@ -24,22 +25,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-#create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-	Fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + this_fruit_choice)
-	fruityvice_normalized = pandas.json_normalize(fruity_response.json())
-	return fruityvice_normalized
-	
-	
 #New Section to display fruityvice api response 
-streamlit.header('Fruityvice Fruit Advice!')
-try: 
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-		streamlit.error("Please select a fruit to get information.")
-else: 
-	back_from_function = get_fruityvice_data(fruit_choice)
-	streamlit.dataframe(back_from_function)
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
@@ -55,6 +53,3 @@ streamlit.dataframe(my_data_rows)
 
 # This will not work correctly, but just go with it for now 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
-
