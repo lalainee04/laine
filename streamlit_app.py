a443ce63c8ad93cd8c659448cb3c6575970d1d18
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-12 17:28:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,8 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()
 
 #import snowflake.connector
 
