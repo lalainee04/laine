de36f56a7ab91c5cf757d07ba9e52be1b2001997
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-12 18:36:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ streamlit.stop()
 
 
 #import snowflake.connector
+# Allow the end user to add a fruit to the list 
+add_my_fruit = streamlit.text_input ('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
