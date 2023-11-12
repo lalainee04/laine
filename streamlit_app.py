8abfc8d21f6e16328f0c622f7c547d99b44fdc5e
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-12 19:52:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
 
+
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+	fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + this_fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())	
+	return fruityvice_normalized
+ 
 #New Section to display fruityvice api response 
 streamlit.header('Fruityvice Fruit Advice!')
-try: 
-	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+try: 		
+	fruit_choice = streamlit.text_input ('What fruit would you like information about?')
 	if not fruit_choice:
-		streamlit.error("Please select a fruit to get information.")
+		streamlit.error("Please select a fruit to get information.")	
 	else: 
-		Fruity_response = requests.get("http://fruityvice.com/api/fruit"+fruit_choice)
-		fruityvice_normalized = pandas.json_normalize(fruity_response.json())
+		back_from_function = get_fruityvice_data(kiwi)
 		streamlit.dataframe(back_from_function)
-
-except URLError as e:
-	streamlit.error 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
