67c5bfd8ee6c2bcb5313a64cd094a1d5e4cda440
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-13 11:21:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,22 +25,12 @@ streamlit.dataframe(my_fruit_list)
 
 #import snowflake.connector
 
-#create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())	
-	return fruityvice_normalized
- 
-#New Section to display fruityvice api response 
-streamlit.header('Fruityvice Fruit Advice!')
-try: 		
-	fruit_choice = streamlit.text_input ('What fruit would you like information about?')
-	if not fruit_choice:
-		streamlit.error("Please select a fruit to get information.")	
-	else: 
-		back_from_function = get_fruityvice_data(fruit_choice)
-		streamlit.dataframe(back_from_function)
-		
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 
 
