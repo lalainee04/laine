2aa13d04434c154a055facafd6dc94ca0397246c
lalainee04 <135224254+lalainee04@users.noreply.github.com>
2023-11-13 16:31:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,6 @@ execept URLError as e:
 # don't run anything past here while we troubleshoot
 streamlit.stop()
 
-
 import snowflake.connector 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
